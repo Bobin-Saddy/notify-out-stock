dca302ff531f6fff3e966b18e5374037483ef6b6
Digittrix Dev <dev@digittrix.in>
2026-01-23 15:49:13 +0530

    update-this

---
+++ prisma/migrations/20260123101839_add_product_details/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "BackInStock" ADD COLUMN "productId" TEXT;
+ALTER TABLE "BackInStock" ADD COLUMN "productTitle" TEXT;
+ALTER TABLE "BackInStock" ADD COLUMN "variantTitle" TEXT;
